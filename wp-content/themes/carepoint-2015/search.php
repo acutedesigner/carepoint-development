7f9fc60fb91593a3d31948fa77c6557d6b6e3c6e
Nigel M Peters <nutyga@gmail.com>
2015-11-16 13:44:15 +0000

    Progressing SearchWP implementation

--- wp-content/themes/carepoint-2015/search.php
+++ wp-content/themes/carepoint-2015/search.php
@@ -6,17 +6,38 @@
  * Use SearchWP's SWP_Query to perform a search using a supplemental engine
  */
 
-$swp_query = new SWP_Query(
-    array(
-        's'      => $_REQUEST['s'],    // search query
-        'engine' => $_REQUEST['search_type'], // search engine
-    )
-);
+if(isset($_REQUEST['cp_term']))
+{
+    $swp_query = new SWP_Query(
+        array(
+            's'      => $_REQUEST['s'],    // search query
+            'engine' => $_REQUEST['search_type'], // search engine
+            'posts_per_page' => 100,
+            'tax_query' => array(       // tax_query support
+                        array(
+                            'taxonomy' => str_replace('_','-',$_REQUEST['search_type']) . '-categories',
+                            'field'    => 'slug',
+                            'terms'    => array( $_REQUEST['cp_term'] ),
+                        ),
+                    ),
+        )
+    );
+}
+else
+{
+    $swp_query = new SWP_Query(
+        array(
+            's'      => $_REQUEST['s'],    // search query
+            'engine' => $_REQUEST['search_type'], // search engine
+            'posts_per_page' => 100
+        )
+    );
+}
 
 ?>
 
     <div class="container">
-        
+        <nav class="breadcrumbs"></nav>
         <div class="block block-intro">
             <h1 class="b-title">Showing results for <?php echo '"'.get_search_query().'"'; ?></h1>
             <h3><?php global $wp_query; echo 'There are '. $wp_query->found_posts.' care guidance results found.'; ?></h3>
@@ -48,8 +69,30 @@ if ( ! empty( $swp_query->posts ) ) {
 
         </div><!-- end of .left-column -->
         <div class="right-column">
-        
-        <?php global $searchwp_categories; echo 'Relevanssi found ' . $wp_query->found_posts . ' hits in categories ' . implode(', ', $searchwp_categories) . '.'; ?>
+
+            <section class="section related-posts">
+                <h2 class="section-header">Filter results by category</h2>
+                <ul class="headline-list">
+                <?php
+
+                    if(!isset($_REQUEST['cp_term']))
+                    {
+                        global $searchwp_categories;
+
+                        foreach($searchwp_categories as $category)
+                        {
+                            echo "<li><h3><a href='". get_site_url() . '/?' . $_SERVER['QUERY_STRING'] . '&cp_term='. $category['cat-slug'] ."'>". $category['cat-name'] ."</a></h3></li>";
+                        }
+                    }
+                    else
+                    {
+                        $url = explode("&cp_term", $_SERVER['QUERY_STRING']);
+                        echo "<li><h3><a href='". get_site_url() . '/?' . $url[0] ."'>Show all results</a></h3></li>";
+                    }
+                ?>      
+                </ul>
+
+            </section>
 
         </div><!-- end of .right-column -->
     </div>
