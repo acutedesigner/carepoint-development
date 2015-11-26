ceb6f551aba046e6575a847b3c1e72535d7bcd05
Nigel M Peters <nutyga@gmail.com>
2015-11-26 13:59:37 +0000

    Updated footer layout/css

--- wp-content/themes/carepoint-2015/footer.php
+++ wp-content/themes/carepoint-2015/footer.php
@@ -6,22 +6,31 @@
 		<div class="container">
 			<p>Get in touch with us on: <a href="tel:01708 776770">01708 776770</a> or online <a href="<?php echo site_url('/feedback-forms'); ?>" class="btn violet-grad">Feedback forms <i class="fa fa-commenting"></i></a></p>	
 		</div>
-	</div><footer class="footer" role="contentinfo">
+	</div>
+	<footer class="footer" role="contentinfo">
 		<div class="container">
-
-			<?php wp_nav_menu( array('theme_location' => 'footer_menu', 'container' => 'nav', 'container_class' => 'footer-nav')); ?>
-
-			<div class="footer-logos">
-				<ul>
-					<li><a href=""><img src="<?php bloginfo("template_directory"); ?>/library/images/accreditation-logo.jpg" alt=""></a></li>
-					<li><a href=""><img src="<?php bloginfo("template_directory"); ?>/library/images/accreditation-logo.jpg" alt=""></a></li>
-					<li><a href=""><img src="<?php bloginfo("template_directory"); ?>/library/images/accreditation-logo.jpg" alt=""></a></li>
-					<li><a href=""><img src="<?php bloginfo("template_directory"); ?>/library/images/accreditation-logo.jpg" alt=""></a></li>	
-				</ul>
-			</div>
-
+			<h3>Care Point Havering</h3>
+			<p>Care Point Family Mosaic, 1st Floor Holgate House, 6 Holgate Court, Western Road, Romford, RM1 3JS</p>
+			<p><strong>Tel:</strong> 01708 776 770<br/><strong>Email:</strong> <a href="mailto:carepoint@familymosaic.co.uk">carepoint@familymosaic.co.uk</a></p>
 		</div>
 	</footer>
+
+	<div class="footer-logos">
+		<div class="container">
+		<h3>Our partners:</h3>
+			<ul>
+				<li><a href=""><img src="<?php bloginfo("template_directory"); ?>/library/images/logo-havering-council.jpg" alt=""></a></li>
+				<li><a href=""><img src="<?php bloginfo("template_directory"); ?>/library/images/logo-family-mosaic.jpg" alt=""></a></li>
+				<li><a href=""><img src="<?php bloginfo("template_directory"); ?>/library/images/logo-nelft-nhs.jpg" alt=""></a></li>
+				<li><a href=""><img src="<?php bloginfo("template_directory"); ?>/library/images/logo-healthwatch.jpg" alt=""></a></li>
+				<li><a href=""><img src="<?php bloginfo("template_directory"); ?>/library/images/logo-havering-ccg.jpg" alt=""></a></li>
+			</ul>
+		</div>
+	</div>
+
+	<nav class="footer-nav" role="navigation">
+		<?php wp_nav_menu( array('theme_location' => 'footer_menu', 'container' => 'div', 'container_class' => 'container')); ?>
+	</nav>
 	<!-- End Footer -->
 
 	<script src="<?php bloginfo("template_directory"); ?>/library/js/jquery.tooltipster.min.js"></script>
